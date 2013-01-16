53ad6209f64bbdb4b7f073f0ab7351322f25014c
Michael C. Mansell <mcmansell1@aol.com>
2013-01-15 21:18:24 -0800

    Initial upload

---
+++ res/scripts/site_functions.js
@@ -0,0 +1,8 @@
+function writedivider() {
+	document.write("<div style=\"line-height: 0px; height: 2px; background: black; margin-bottom: 2px;\"></div>");
+	document.write("<div style=\"line-height: 0px; height: 1px; background: black; margin-bottom: 2px;\"></div>");
+	document.write("<div style=\"line-height: 0px; height: 2px; background: black;\"></div>");
+}
+function writesecdivider() {
+	document.write("<p><div style=\"height: 1px; background: black;\"></div></p>");
+}
